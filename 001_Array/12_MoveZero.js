f8147ae1ac553055759f0753ace2004ca67a973a
aneeshBakshi <aneesh.bakshi1@gmail.com>
2023-08-22 10:51:14 +0530

    Optimised 001_Array/12_MoveZero.js logic

--- 001_Array/12_MoveZero.js
+++ 001_Array/12_MoveZero.js
@@ -1,16 +1,36 @@
 // Move all Zeroes to the end of the array
 
 // Time complexity is O(n+m) which is O(x) and auxillary space complexity is O(m)
+// function moveZero(arr) {
+//   const arr2 = [];
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] !== 0) {
+//       arr2.push(arr[i]);
+//       arr[i] = 0;
+//     }
+//   }
+//   for (let j = 0; j < arr2.length; j++) {
+//     arr[j] = arr2[j];
+//   }
+//   return arr;
+// }
+
+// Optimised Solution
+
+// Time Complexity is O(n) and Auxillary Space Complexity is O(1)
 function moveZero(arr) {
-  const arr2 = [];
+  let count = 0;
+  let temp;
   for (let i = 0; i < arr.length; i++) {
     if (arr[i] !== 0) {
-      arr2.push(arr[i]);
-      arr[i] = 0;
+      // Swapping logic
+      temp = arr[count];
+      arr[count] = arr[i];
+      arr[i] = temp;
+
+      // Increment Count
+      count++;
     }
-  }
-  for (let j = 0; j < arr2.length; j++) {
-    arr[j] = arr2[j];
   }
   return arr;
 }
